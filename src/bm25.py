523bd8a924fb82d782abb05de7084c86e827a43e
Francesc0rtu <francescortu@live.it>
2023-02-04 10:41:40 +0100

    update

--- src/bm25.py
+++ src/bm25.py
@@ -1,21 +1,30 @@
-
+from text_processing import *
 
 class Posting():
-    def __init__(self):
-        self.doc_id = None
-        self.tf = None
+    def __init__(self, doc_id):
+        self.doc_id = doc_id
+        self.tf = 1
+    
+    def update_tf(self ):
+        self.tf = self.tf + 1
 
 class Posting_list():
     def __init__(self, doc_id):
-        self.posting_list = [doc_id]
+        self.posting_list = [Posting(doc_id)]
 
     def append(self, doc_id):
-        self.posting_list.append(doc_id)
+        self.posting_list.append(Posting(doc_id))
+    
+    def get_Posting(self, doc_id):
+        for post in self.posting_list:
+            if post.doc_id == doc_id:
+                return post
+        
+        return None
+
 
 
 class Term():
-    ''' 
-    '''
 
     def __init__(self, term, doc_id):
         self.term = term
@@ -24,8 +33,15 @@ class Term():
     
     def __str__(self):
         return f"term: {self.term}, df: {self.df}"
+    
+    def check_Posting(self, doc_id):
+        posting  = self.posting_list.get_Posting(doc_id)
+        if posting is None:
+            self.add_Posting(doc_id)
+        else:
+            posting.update_tf()
 
-    def add_posting(self, doc_id):
+    def add_Posting(self, doc_id):
         self.posting_list.append(doc_id)
     
 
@@ -34,3 +50,7 @@ class Index():
     def __init__(self):
         self.dict = {}  # * token -> Term(  ,   )
         self.doc_len = {}
+
+    def Create_Index(self, corpus):
+        
+

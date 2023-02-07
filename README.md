409667338c17673e083e4ec6d8a52d89537c267c
Francesc0rtu <francescortu@live.it>
2023-02-07 21:12:31 +0100

    upload init of doc2vec

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # information_retrieval
 code used during lectures of information retrieval @ dssc-units
+
+
+# TODO
+
+## 1. Implement query
--- src/ir_system.py
+++ src/ir_system.py
@@ -3,6 +3,7 @@ from collections import defaultdict, OrderedDict
 import math
 from src.utils import Heap
 from tqdm import tqdm
+import numpy as np
 
 
 class IRSystem():
@@ -14,13 +15,56 @@ class IRSystem():
 
     def search(self, query):
         """This function return the top 10 documents of a query"""
-        
+        query_vector = self.get_query_vector(query)
+
+    def get_query_vector(self, query):
+        query_vector = np.zeros(len(self.index))
+        for term in query:
+            if term in self.index:
+                query_vector[self.index[term].position] = 1
+
+        # normalize
+        query_vector = query_vector/np.linalg.norm(query_vector)
+        return query_vector
+    
+    def get_doc_vectors(self):
+        doc_vectors = []
+        for docid in range(self.N):
+            doc_vectors.append(self.get_doc_vector(docid))
+        return doc_vectors
+
+    def get_doc_vector(self, docid):
+        doc_vector = np.zeros(len(self.index))
+        for term in self.corpus[docid]:
+            if term in self.index:
+                doc_vector[self.index[term].position] = self.index.tf_idf(term, docid, self.N)
+        # normalize
+        doc_vector = doc_vector/np.linalg.norm(doc_vector)            
+        return doc_vector
+   
+    def get_doc_vector_from_query(self, query):
+        vectors = {}
+        for term in query:
+            if term in self.index:
+                for docid, _ in self.index[term]:
+                    if docid in vectors:
+                        vectors[docid][self.index[term].position()] = self.index.tf_idf(term, docid, self.N)
+                    else:
+                        vectors[docid] = np.zeros(len(self.index))
+                        vectors[docid][self.index[term].position()] = self.index.tf_idf(term, docid, self.N)
+        # normalize
+        for docid in vectors:
+            if np.linalg.norm(vectors[docid]) > 0:
+              vectors[docid] = vectors[docid]/np.linalg.norm(vectors[docid])
+        return vectors
+    
 
 
 class Posting_list():
-    def __init__(self):
+    def __init__(self, position):
         self._postings = []
         self._df = 0
+        self._position = position
 
     def __repr__(self):
         return str(self._postings)
@@ -30,10 +74,10 @@ class Posting_list():
 
     def add_posting(self, docid):
         index = self.find_posting_index(docid)
-        self._df += 1
         if index is not None:
             self._postings[index] = (docid, self._postings[index][1] + 1)
         else:
+            self._df += 1
             index = len(self._postings)
             self._postings.append((docid, 1))
 
@@ -49,11 +93,18 @@ class Posting_list():
             if posting[0] == docid:
                 return index
         return None
+
+    def position(self):
+        return self._position
+    
+    def get_post(self, docid):
+        index = self.find_posting_index(docid)
+        return self._postings[index]
     
 
 class Index():
     def __init__(self, corpus):
-        self._terms = {}
+        self._terms = OrderedDict()
         self.make_index(corpus)
     
     def __repr__(self):
@@ -71,26 +122,33 @@ class Index():
             return True
         except KeyError:
             return False
+        
+    def __len__(self):
+        return len(self._terms)
+    
 
     def df(self, term):
         return self._terms[term]._df
     
     def idf(self, term, N):
-        return math.log(N/self.df(term))
+        return math.log10(N/self.df(term))
     
     def tf(self, term, docid):
-        return self._terms[term].find_posting_index(docid).tf()
+        _, tf = self._terms[term].get_post(docid)
+        return tf
     
     def tf_idf(self, term, docid, N):
         return self.tf(term, docid)*self.idf(term, N)
         
     def make_index(self, corpus):
+        position = 0
         for docid, doc in tqdm(enumerate(corpus), total=len(corpus)):
             for term in doc:
                 try:
                     self._terms[term].add_posting(docid)
                 except KeyError:
-                    self._terms[term] = Posting_list()
+                    self._terms[term] = Posting_list(position)
+                    position += 1
                     self._terms[term].add_posting(docid)
     
 
